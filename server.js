3756fa93099e8f4d0c1cf9707401421b66c24030
Pasang Tamang <pasanghilp@gmail.com>
2026-09-23 11:07:21 +0545

    fix: correct title field in posts array and remove unused login route

--- server.js
+++ server.js
@@ -13,22 +13,12 @@ const posts = [
     },
     {
     username: 'Djalok',
-    age: 'Post 2'
+    title: 'Post 2'
     }
 ]
 
 app.get('/posts', authenticateToken, (req, res)=>{
     res.json(posts.filter(post => post.username === req.user.name))
-});
-
-app.post('/login', (req,res)=>{
-    //user authentication
-
-    const username = req.body.username;
-    const user = { name: username}
-
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-    res.json({ accessToken: accessToken})
 });
 
 function authenticateToken(req,res, next){
